dbf7d3c9dc8379266f7a5f4879abca9a90a55644
Justin Hall <justin.r.hall@gmail.com>
2019-01-23 14:50:22 -0700

    Add Typography.js with Moraga theme

---
+++ src/utils/typography.ts
@@ -0,0 +1,7 @@
+import Typography from 'typography';
+import moraga from 'typography-theme-moraga';
+
+const typography = new Typography(moraga);
+
+export const { rhythm, scale } = typography;
+export default typography;
