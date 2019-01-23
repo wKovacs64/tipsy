aa900174e2c835ea60caeddeffa94ac1d1f27b1a
Justin Hall <justin.r.hall@gmail.com>
2019-01-23 00:33:46 -0700

    Initial

---
+++ config/site.js
@@ -0,0 +1,9 @@
+const title = 'Nip the Tip';
+const description = 'A simple tip calculator and bill divider';
+const pwaShortName = 'Nip the Tip';
+
+module.exports = {
+  title,
+  description,
+  pwaShortName,
+};
