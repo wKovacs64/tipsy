54b837cc3cfc4cb6a0e1404054cf72537a7b1065
Justin Hall <justin.r.hall@gmail.com>
2019-01-23 14:50:22 -0700

    Add Typography.js with Moraga theme

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,12 @@ module.exports = {
   plugins: [
     'gatsby-plugin-typescript',
     {
+      resolve: 'gatsby-plugin-typography',
+      options: {
+        pathToConfigModule: 'src/utils/typography.ts',
+      },
+    },
+    {
       resolve: 'gatsby-plugin-netlify',
       options: {
         headers: {
