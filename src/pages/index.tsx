89cb329f79ec0b1c3d0925fdd72b3c4d0ce5e647
Justin Hall <justin.r.hall@gmail.com>
2019-02-04 16:24:50 -0700

    Update button styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -45,17 +45,19 @@ const Button = styled.button`
   display: flex;
   align-items: center;
   justify-content: space-evenly;
-  background-color: transparent;
+  color: white;
+  background-color: ${colors.main};
   border: none;
-  box-shadow: 2px 2px 4px 2px rgba(0, 0, 0, 0.2);
+  box-shadow: 4px 4px 8px 0px rgba(0, 0, 0, 0.2);
   cursor: pointer;
-  height: ${scale(1.5).lineHeight};
+  padding: ${rhythm(1.25)};
   font-weight: 200;
   font-size: ${scale(1.25).fontSize};
-  width: 60vw;
+  width: 75vw;
   max-width: ${rhythm(18)};
   ${mq.sm} {
-    font-size: ${scale(0.75).fontSize};
+    font-size: ${scale(0.5).fontSize};
+    padding: ${rhythm(0.25)};
   }
 `;
 
