32030182123164df7ad5158e3c544c64ee6f24b1
Justin Hall <justin.r.hall@gmail.com>
2019-02-05 23:09:53 -0700

    Add shell for CalcPage

---
+++ src/pages/calc.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+// import styled from '@emotion/styled';
+// import { scale } from '../utils/typography';
+import Layout from '../components/layout';
+
+const CalcPage: React.FunctionComponent = () => <Layout>{/* TODO */}</Layout>;
+
+export default CalcPage;
