56d12c1edd5b3b3d1464aa9a7df2c6ecfc107a62
Justin Hall <justin.r.hall@gmail.com>
2019-02-05 23:04:21 -0700

    Update NotFoundPage

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import styled from '@emotion/styled';
+import { scale } from '../utils/typography';
 import Layout from '../components/layout';
+
+const Centered = styled.section`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const P = styled.p`
+  font-weight: 200;
+  font-size: ${scale(1.25).fontSize};
+`;
 
 const NotFoundPage: React.FunctionComponent = () => (
   <Layout>
-    <p>Dead link!</p>
+    <Centered>
+      <P>Nope.</P>
+    </Centered>
   </Layout>
 );
 
