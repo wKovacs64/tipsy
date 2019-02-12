c610b583d65e27f872a1b45c8060c6f727c95e42
Justin Hall <justin.r.hall@gmail.com>
2019-02-12 16:22:27 -0700

    Fix LinkButton styles

--- src/styles/link-button.tsx
+++ src/styles/link-button.tsx
@@ -13,15 +13,18 @@ const LinkButton = styled(Link)`
   border: none;
   box-shadow: 4px 4px 8px 0px rgba(0, 0, 0, 0.2);
   cursor: pointer;
-  padding: ${rhythm(1.25)};
+  padding: ${rhythm(0.4)};
   font-weight: 200;
-  font-size: ${scale(1.25).fontSize};
+  font-size: ${scale(1).fontSize};
+  line-height: ${scale(1).lineHeight};
   width: 100%;
   &:hover {
+    color: white;
     text-decoration: none;
   }
   ${mq.sm} {
     font-size: ${scale(0.5).fontSize};
+    line-height: ${scale(0.5).lineHeight};
     padding: ${rhythm(0.25)};
   }
 `;
