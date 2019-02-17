05177b146f869e4469deadb6fb5afb54c0b38ae3
Justin Hall <justin.r.hall@gmail.com>
2019-02-16 23:13:50 -0700

    Blur NumericInput when done editing to hide mobile soft keyboard

--- src/components/numeric-input.tsx
+++ src/components/numeric-input.tsx
@@ -3,6 +3,22 @@ import NumberInput from '../styles/number-input';
 
 const NumericInput: React.FunctionComponent<
   React.InputHTMLAttributes<HTMLInputElement>
-> = props => <NumberInput type="text" inputMode="numeric" {...props} />;
+> = props => {
+  const ref = React.useRef<HTMLInputElement>(null);
+
+  return (
+    <NumberInput
+      type="text"
+      inputMode="numeric"
+      ref={ref}
+      onKeyDown={e => {
+        if (e.keyCode === 13 && ref && ref.current) {
+          ref.current.blur();
+        }
+      }}
+      {...props}
+    />
+  );
+};
 
 export default NumericInput;
