6597d667b094c95194fefba162d93c1d27db0bd6
Justin Hall <justin.r.hall@gmail.com>
2019-02-17 12:45:17 -0700

    Fix ThemedIconButton padding

--- src/styles/themed-icon-button.tsx
+++ src/styles/themed-icon-button.tsx
@@ -1,8 +1,10 @@
 import styled from '@emotion/styled';
+import { rhythm } from '../utils/typography';
 import { palette, light } from '../theme';
 import IconButton from './icon-button';
 
 const ThemedIconButton = styled(IconButton)`
+  padding: ${rhythm(0.25)};
   body.light-mode & {
     color: ${light.text};
   }
