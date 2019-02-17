bb8820710db0b581721c7966178a449b879acbca
Justin Hall <justin.r.hall@gmail.com>
2019-02-17 09:15:02 -0700

    Increase padding in BrandButton

--- src/styles/brand-button.tsx
+++ src/styles/brand-button.tsx
@@ -1,6 +1,5 @@
 import styled from '@emotion/styled';
 import colors from '../utils/colors';
-import mq from '../utils/mq';
 import { rhythm, scale } from '../utils/typography';
 
 const BrandButton = styled.button`
@@ -12,14 +11,11 @@ const BrandButton = styled.button`
   border: none;
   box-shadow: 4px 4px 8px 0px rgba(0, 0, 0, 0.2);
   cursor: pointer;
-  padding: ${rhythm(0.2)};
+  padding: ${rhythm(0.25)};
   font-weight: 200;
   font-size: ${scale(0.5).fontSize};
   line-height: ${scale(0.5).lineHeight};
   width: 100%;
-  ${mq.sm} {
-    padding: ${rhythm(0.25)};
-  }
   &:disabled {
     cursor: not-allowed;
     opacity: 0.65;
