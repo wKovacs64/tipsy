8d17b9de11d614fa89bfad0193bd10b5c8bb36c8
Justin Hall <justin.r.hall@gmail.com>
2019-02-18 14:15:44 -0700

    Unset overflow-y on html element

--- src/utils/typography.ts
+++ src/utils/typography.ts
@@ -1,6 +1,12 @@
 import Typography from 'typography';
 import moraga from 'typography-theme-moraga';
 
+moraga.overrideThemeStyles = () => ({
+  html: {
+    overflowY: 'unset',
+  },
+});
+
 const typography = new Typography(moraga);
 
 export const { rhythm, scale } = typography;
