a83812e75d406fbe918558243b6298cc11620592
Justin Hall <justin.r.hall@gmail.com>
2019-02-18 14:15:03 -0700

    Refactor IndexPage E2E tests

--- cypress/e2e/index-page.spec.ts
+++ cypress/e2e/index-page.spec.ts
@@ -2,22 +2,20 @@ describe('Index Page', () => {
   beforeEach(() => {
     cy.server();
     cy.visit('/').injectAxe();
+    // wait for the content to ensure the app has been rendered
+    cy.get('html[lang="en"]');
   });
 
-  it('has no detectable a11y violations in light mode', () => {
-    // wait for the content to ensure the app has been rendered
-    cy.get('html[lang="en"]')
-      .getByLabelText(/bill/i)
-      .checkA11y();
+  it('has no detectable a11y violations (light mode)', () => {
+    cy.getByLabelText(/bill/i).checkA11y();
     cy.getByLabelText(/bill/i)
       .click()
       .type('12345')
       .checkA11y();
   });
 
-  it('has no detectable a11y violations in dark mode', () => {
-    // wait for the content to ensure the app has been rendered
-    cy.get('html[lang="en"]').getByLabelText(/bill/i);
+  it('has no detectable a11y violations (dark mode)', () => {
+    cy.getByLabelText(/bill/i);
     cy.toggleDarkMode();
     cy.checkA11y();
     cy.getByLabelText(/bill/i)
