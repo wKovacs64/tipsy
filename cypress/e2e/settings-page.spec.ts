da623d9448f71907a04a0c90aa17e27be7addd30
Justin Hall <justin.r.hall@gmail.com>
2019-02-19 20:02:07 -0700

    Add more SettingsPage E2E tests

--- cypress/e2e/settings-page.spec.ts
+++ cypress/e2e/settings-page.spec.ts
@@ -1,3 +1,15 @@
+function fillOutSettings(): Cypress.Chainable {
+  return cy
+    .getByLabelText(/default party size/i)
+    .should('not.have.value', '24')
+    .click()
+    .type('24{enter}')
+    .getByLabelText(/default tip percentage/i)
+    .should('not.have.value', '42')
+    .click()
+    .type('42{enter}');
+}
+
 describe('Settings Page', () => {
   beforeEach(() => {
     cy.server();
@@ -19,5 +31,39 @@ describe('Settings Page', () => {
   it('has no detectable a11y violations (dark mode)', () => {
     cy.getByLabelText(/dark mode/i).click({ force: true });
     // cy.checkA11y();
+  });
+
+  it('toggles dark mode', () => {
+    cy.get('body')
+      .should('have.class', 'light-mode')
+      .getByLabelText(/dark mode:/i)
+      .click({ force: true })
+      .get('body')
+      .should('have.class', 'dark-mode')
+      .getByLabelText(/dark mode:/i)
+      .click({ force: true })
+      .get('body')
+      .should('have.class', 'light-mode');
+  });
+
+  it('persists numeric options only on save', () => {
+    fillOutSettings()
+      .visit('/')
+      .getByLabelText(/settings/i)
+      .click()
+      .getByLabelText(/default party size/i)
+      .should('not.have.value', '24')
+      .getByLabelText(/default tip percentage/i)
+      .should('not.have.value', '42')
+      .getByLabelText(/default party size/i);
+    fillOutSettings()
+      .getByText(/save/i)
+      .click()
+      .getByLabelText(/settings/i)
+      .click()
+      .getByLabelText(/default party size/i)
+      .should('have.value', '24')
+      .getByLabelText(/default tip percentage/i)
+      .should('have.value', '42');
   });
 });
