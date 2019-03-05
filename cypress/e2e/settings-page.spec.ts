6375cfec25015130d16e52ec70ba03adf95d86a5
Justin Hall <justin.r.hall@gmail.com>
2019-03-04 19:57:27 -0700

    Enable a11y tests for SettingsPage

--- cypress/e2e/settings-page.spec.ts
+++ cypress/e2e/settings-page.spec.ts
@@ -13,24 +13,27 @@ function fillOutSettings(): Cypress.Chainable {
 describe('Settings Page', () => {
   beforeEach(() => {
     cy.visit('/settings').injectAxe();
+    cy.configureAxe({
+      rules: [
+        {
+          // https://github.com/markusenglund/react-switch/issues/34
+          id: 'aria-required-attr',
+          enabled: false,
+        },
+      ],
+    });
     // wait for the content to ensure the app has been rendered
     cy.get('html[lang="en"]');
   });
 
-  // TODO: enable cy.checkA11y() calls once cypress-axe is configurable and we
-  // can disable the over-zealous `aria-required-attr` rule. Alternatively, we
-  // could leverage patch-package to add `aria-checked={checked}` to the `input`
-  // component rendered by react-switch, which is what's causing the
-  // false-positive.
+  it('has no detectable a11y violations (light mode)', () => {
+    cy.checkA11y();
+  });
 
-  // it('has no detectable a11y violations (light mode)', () => {
-  //   cy.checkA11y();
-  // });
-
-  // it('has no detectable a11y violations (dark mode)', () => {
-  //   cy.getByLabelText(/dark mode/i).click({ force: true });
-  //   cy.checkA11y();
-  // });
+  it('has no detectable a11y violations (dark mode)', () => {
+    cy.getByLabelText(/dark mode/i).click({ force: true });
+    cy.checkA11y();
+  });
 
   it('toggles dark mode', () => {
     cy.get('body')
