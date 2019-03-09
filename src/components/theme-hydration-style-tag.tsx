871060155c19a55ef41d5afcd26b0a550daf0d9a
Justin Hall <justin.r.hall@gmail.com>
2019-03-09 11:50:39 -0700

    Bring in gatsby-plugin-use-dark-mode

---
+++ src/components/theme-hydration-style-tag.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { light, dark } from '../theme';
+
+const hydrateThemeCss = `
+  body.light-mode {
+    color: ${light.text};
+    background-color: ${light.background};
+  }
+  body.dark-mode {
+    color: ${dark.text};
+    background-color: ${dark.background};
+  }
+`.replace(/\s+/g, '');
+
+const ThemeHydrationStyleTag: React.FunctionComponent = () => (
+  // eslint-disable-next-line react/no-danger
+  <style dangerouslySetInnerHTML={{ __html: hydrateThemeCss }} />
+);
+
+export default ThemeHydrationStyleTag;
