ba9f51067675c6718a93853d1f0eb9aee0276ab7
Justin Hall <justin.r.hall@gmail.com>
2021-02-02 17:28:12 -0700

    refactor: add temporary type assertion for location with state

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,33 +1,31 @@
 import { css } from '@emotion/react';
 import { FaGithub } from 'react-icons/fa';
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { Location } from '@reach/router'; // comes with Gatsby
+import { useLocation } from '@reach/router'; // comes with Gatsby
 import { rhythm } from '../theme';
 import A from './external-link';
 
-const Footer: React.FunctionComponent = () => (
-  <Location>
-    {({ location }) => {
-      if (location.pathname === '/') {
-        return (
-          <footer
-            css={css`
-              display: flex;
-              align-items: center;
-              justify-content: center;
-              padding: ${rhythm(1)};
-            `}
-          >
-            <A href="https://github.com/wKovacs64/tipsy">
-              <FaGithub aria-label="View source on GitHub" size={32} />
-            </A>
-          </footer>
-        );
-      }
+const Footer: React.FunctionComponent = () => {
+  const location = useLocation();
 
-      return <footer />;
-    }}
-  </Location>
-);
+  if (location.pathname === '/') {
+    return (
+      <footer
+        css={css`
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          padding: ${rhythm(1)};
+        `}
+      >
+        <A href="https://github.com/wKovacs64/tipsy">
+          <FaGithub aria-label="View source on GitHub" size={32} />
+        </A>
+      </footer>
+    );
+  }
+
+  return <footer />;
+};
 
 export default Footer;
