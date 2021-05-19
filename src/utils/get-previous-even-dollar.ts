611971f63c7acec43f01cfe0562bd0a6622b924a
Justin Hall <justin.r.hall@gmail.com>
2021-05-19 14:46:59 -0600

    chore(deps): update usage of currency.js types

--- src/utils/get-previous-even-dollar.ts
+++ src/utils/get-previous-even-dollar.ts
@@ -1,2 +1,4 @@
+import type currency from 'currency.js';
+
 export const getPreviousEvenDollar = (current: currency): number =>
   current.cents() ? current.dollars() : current.subtract(1).dollars();
