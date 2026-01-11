245e4260b91adec3a0b921078fe501ee71b23d82
Justin Hall <justin.r.hall@gmail.com>
2026-01-10 23:16:47 -0700

    refactor(calc): simplify calculator logic

--- src/pages/calc.tsx
+++ src/pages/calc.tsx
@@ -8,52 +8,48 @@ import { billFromUrlParam, getPreviousEvenDollar, toCurrency, toNumber } from '.
 import BrandButton from '../shared/brand-button';
 import NumericInput from '../shared/numeric-input';
 
-enum ActionType {
-  CHANGE_TIP_PERCENT,
-  CHANGE_TIP_AMOUNT,
-  CHANGE_TOTAL_AMOUNT,
-  CHANGE_NUMBER_OF_PEOPLE,
-  CHANGE_EACH_PERSON_PAYS,
-}
+type TipSource =
+  | { type: 'percent'; value: number }
+  | { type: 'amount'; value: number }
+  | { type: 'total'; value: number }
+  | { type: 'perPerson'; value: number };
 
-interface ChangeTipPercent {
-  type: ActionType.CHANGE_TIP_PERCENT;
-  payload: number;
-}
-
-interface ChangeTipAmount {
-  type: ActionType.CHANGE_TIP_AMOUNT;
-  payload: number;
-}
-
-interface ChangeTotalAmount {
-  type: ActionType.CHANGE_TOTAL_AMOUNT;
-  payload: number;
-}
-
-interface ChangeNumberOfPeople {
-  type: ActionType.CHANGE_NUMBER_OF_PEOPLE;
-  payload: number;
-}
-
-interface ChangeEachPersonPays {
-  type: ActionType.CHANGE_EACH_PERSON_PAYS;
-  payload: number;
-}
-
-type Action =
-  | ChangeTipPercent
-  | ChangeTipAmount
-  | ChangeTotalAmount
-  | ChangeNumberOfPeople
-  | ChangeEachPersonPays;
-
-interface State {
-  tipPercent: number;
-  tipAmount: number;
-  totalAmount: number;
+type State = {
+  tipSource: TipSource;
   numberOfPeople: number;
-  eachPersonPays: number;
+};
+
+function deriveValues(billAmount: number, state: State) {
+  const { tipSource, numberOfPeople } = state;
+
+  if (billAmount <= 0 || numberOfPeople <= 0) {
+    return { tipPercent: 0, tipAmount: 0, totalAmount: billAmount, eachPersonPays: 0 };
+  }
+
+  let tipAmount: number;
+  switch (tipSource.type) {
+    case 'percent':
+      tipAmount = currency(billAmount).multiply(tipSource.value).divide(100).value;
+      break;
+    case 'amount':
+      tipAmount = tipSource.value;
+      break;
+    case 'total':
+      tipAmount = currency(tipSource.value).subtract(billAmount).value;
+      break;
+    case 'perPerson':
+      tipAmount = currency(tipSource.value).multiply(numberOfPeople).subtract(billAmount).value;
+      break;
+  }
+
+  const totalAmount = currency(billAmount).add(tipAmount).value;
+
+  return {
+    tipPercent: currency(tipAmount).divide(billAmount).multiply(100).value,
+    tipAmount,
+    totalAmount,
+    eachPersonPays: currency(totalAmount).distribute(numberOfPeople)[0].value,
+  };
 }
 
 function CalcPage() {
@@ -65,91 +61,13 @@ function CalcPage() {
   const [initialTipPercent] = useDefaultTipPercent();
 
   const billAmount = Number.parseFloat(bill) || 0;
-  const initialTipAmount = currency(billAmount).multiply(initialTipPercent).divide(100).value;
-  const initialTotalAmount = currency(billAmount).add(initialTipAmount).value;
-  const initialEachPersonPays = currency(initialTotalAmount).distribute(initialPartySize)[0].value;
 
-  const initialState: State = {
-    tipPercent: initialTipPercent,
-    tipAmount: initialTipAmount,
-    totalAmount: initialTotalAmount,
+  const [state, setState] = React.useState<State>({
+    tipSource: { type: 'percent', value: initialTipPercent },
     numberOfPeople: initialPartySize,
-    eachPersonPays: initialEachPersonPays,
-  };
+  });
 
-  const reducer = (state: State, action: Action): State => {
-    switch (action.type) {
-      case ActionType.CHANGE_TIP_PERCENT: {
-        const tipAmount = currency(action.payload).divide(100).multiply(billAmount).value;
-        const totalAmount = currency(billAmount).add(tipAmount).value;
-        const eachPersonPays = currency(totalAmount).distribute(state.numberOfPeople)[0].value;
-
-        return {
-          tipPercent: action.payload,
-          tipAmount,
-          totalAmount,
-          numberOfPeople: state.numberOfPeople,
-          eachPersonPays,
-        };
-      }
-
-      case ActionType.CHANGE_TIP_AMOUNT: {
-        const tipPercent = currency(action.payload).divide(billAmount).multiply(100).value;
-        const totalAmount = currency(billAmount).add(action.payload).value;
-        const eachPersonPays = currency(totalAmount).distribute(state.numberOfPeople)[0].value;
-
-        return {
-          tipPercent,
-          tipAmount: action.payload,
-          totalAmount,
-          numberOfPeople: state.numberOfPeople,
-          eachPersonPays,
-        };
-      }
-
-      case ActionType.CHANGE_TOTAL_AMOUNT: {
-        const tipAmount = currency(action.payload).subtract(billAmount).value;
-        const tipPercent = currency(tipAmount).divide(billAmount).multiply(100).value;
-        const eachPersonPays = currency(action.payload).distribute(state.numberOfPeople)[0].value;
-
-        return {
-          tipPercent,
-          tipAmount,
-          totalAmount: action.payload,
-          numberOfPeople: state.numberOfPeople,
-          eachPersonPays,
-        };
-      }
-
-      case ActionType.CHANGE_NUMBER_OF_PEOPLE: {
-        const eachPersonPays = currency(state.totalAmount).distribute(action.payload)[0].value;
-
-        return {
-          ...state,
-          numberOfPeople: action.payload,
-          eachPersonPays,
-        };
-      }
-
-      case ActionType.CHANGE_EACH_PERSON_PAYS: {
-        const totalAmount = currency(action.payload).multiply(state.numberOfPeople).value;
-        const tipAmount = currency(totalAmount).subtract(billAmount).value;
-        const tipPercent = currency(tipAmount).divide(billAmount).multiply(100).value;
-
-        return {
-          tipPercent,
-          tipAmount,
-          totalAmount,
-          numberOfPeople: state.numberOfPeople,
-          eachPersonPays: action.payload,
-        };
-      }
-      default:
-        throw new Error('Unrecognized state reducer action type!');
-    }
-  };
-
-  const [state, dispatch] = React.useReducer(reducer, initialState);
+  const { tipPercent, tipAmount, totalAmount, eachPersonPays } = deriveValues(billAmount, state);
 
   const startOver = () => {
     void navigate('/', { replace: true });
@@ -165,30 +83,30 @@ function CalcPage() {
           <DecrementButton
             aria-label="decrement tip percent"
             onClick={() => {
-              dispatch({
-                type: ActionType.CHANGE_TIP_PERCENT,
-                payload: state.tipPercent < 1 ? 0 : state.tipPercent - 1,
-              });
+              setState((previousState) => ({
+                ...previousState,
+                tipSource: { type: 'percent', value: tipPercent < 1 ? 0 : tipPercent - 1 },
+              }));
             }}
           />
           <CalcInput
             id="tip-percent"
             name="tip-percent"
-            value={state.tipPercent}
+            value={tipPercent}
             onChange={(e) => {
-              dispatch({
-                type: ActionType.CHANGE_TIP_PERCENT,
-                payload: toNumber(e.target.value),
-              });
+              setState((previousState) => ({
+                ...previousState,
+                tipSource: { type: 'percent', value: toNumber(e.target.value) },
+              }));
             }}
           />
           <IncrementButton
             aria-label="increment tip percent"
             onClick={() => {
-              dispatch({
-                type: ActionType.CHANGE_TIP_PERCENT,
-                payload: state.tipPercent + 1,
-              });
+              setState((previousState) => ({
+                ...previousState,
+                tipSource: { type: 'percent', value: tipPercent + 1 },
+              }));
             }}
           />
         </Cell>
@@ -199,32 +117,35 @@ function CalcPage() {
           <DecrementButton
             aria-label="decrement tip amount"
             onClick={() => {
-              const current = currency(state.tipAmount, { increment: 1 });
+              const current = currency(tipAmount, { increment: 1 });
               const previousEvenDollar = getPreviousEvenDollar(current);
-              dispatch({
-                type: ActionType.CHANGE_TIP_AMOUNT,
-                payload: current.value < 1 ? 0 : previousEvenDollar,
-              });
+              setState((previousState) => ({
+                ...previousState,
+                tipSource: { type: 'amount', value: current.value < 1 ? 0 : previousEvenDollar },
+              }));
             }}
           />
           <CalcInput
             id="tip-amount"
             name="tip-amount"
-            value={currency(state.tipAmount, { symbol: '' }).format()}
+            value={currency(tipAmount, { symbol: '' }).format()}
             onChange={(e) => {
-              dispatch({
-                type: ActionType.CHANGE_TIP_AMOUNT,
-                payload: toNumber(toCurrency(e.target.value)),
-              });
+              setState((previousState) => ({
+                ...previousState,
+                tipSource: { type: 'amount', value: toNumber(toCurrency(e.target.value)) },
+              }));
             }}
           />
           <IncrementButton
             aria-label="increment tip amount"
             onClick={() => {
-              dispatch({
-                type: ActionType.CHANGE_TIP_AMOUNT,
-                payload: currency(state.tipAmount, { increment: 1 }).add(1).dollars(),
-              });
+              setState((previousState) => ({
+                ...previousState,
+                tipSource: {
+                  type: 'amount',
+                  value: currency(tipAmount, { increment: 1 }).add(1).dollars(),
+                },
+              }));
             }}
           />
         </Cell>
@@ -235,34 +156,39 @@ function CalcPage() {
           <DecrementButton
             aria-label="decrement total amount"
             onClick={() => {
-              const current = currency(state.totalAmount, { increment: 1 });
+              const current = currency(totalAmount, { increment: 1 });
               const previousEvenDollar = getPreviousEvenDollar(current);
-              dispatch({
-                type: ActionType.CHANGE_TOTAL_AMOUNT,
-                payload: previousEvenDollar < billAmount ? billAmount : previousEvenDollar,
-              });
+              setState((previousState) => ({
+                ...previousState,
+                tipSource: {
+                  type: 'total',
+                  value: previousEvenDollar < billAmount ? billAmount : previousEvenDollar,
+                },
+              }));
             }}
           />
           <CalcInput
             id="total-amount"
             name="total-amount"
             className="font-semibold"
-            value={currency(state.totalAmount, { symbol: '' }).format()}
+            value={currency(totalAmount, { symbol: '' }).format()}
             onChange={(e) => {
-              dispatch({
-                type: ActionType.CHANGE_TOTAL_AMOUNT,
-                // TODO: deal with manually setting below billAmount
-                payload: toNumber(toCurrency(e.target.value)),
-              });
+              setState((previousState) => ({
+                ...previousState,
+                tipSource: { type: 'total', value: toNumber(toCurrency(e.target.value)) },
+              }));
             }}
           />
           <IncrementButton
             aria-label="increment total amount"
             onClick={() => {
-              dispatch({
-                type: ActionType.CHANGE_TOTAL_AMOUNT,
-                payload: currency(state.totalAmount, { increment: 1 }).add(1).dollars(),
-              });
+              setState((previousState) => ({
+                ...previousState,
+                tipSource: {
+                  type: 'total',
+                  value: currency(totalAmount, { increment: 1 }).add(1).dollars(),
+                },
+              }));
             }}
           />
         </HeroCell>
@@ -273,10 +199,11 @@ function CalcPage() {
           <DecrementButton
             aria-label="decrement number of people"
             onClick={() => {
-              dispatch({
-                type: ActionType.CHANGE_NUMBER_OF_PEOPLE,
-                payload: state.numberOfPeople < 2 ? 1 : state.numberOfPeople - 1,
-              });
+              setState((previousState) => ({
+                ...previousState,
+                numberOfPeople:
+                  previousState.numberOfPeople < 2 ? 1 : previousState.numberOfPeople - 1,
+              }));
             }}
           />
           <CalcInput
@@ -284,20 +211,19 @@ function CalcPage() {
             name="number-of-people"
             value={state.numberOfPeople}
             onChange={(e) => {
-              dispatch({
-                type: ActionType.CHANGE_NUMBER_OF_PEOPLE,
-                // TODO: deal with manually setting to 0
-                payload: toNumber(e.target.value),
-              });
+              setState((previousState) => ({
+                ...previousState,
+                numberOfPeople: Math.max(1, toNumber(e.target.value)),
+              }));
             }}
           />
           <IncrementButton
             aria-label="increment number of people"
             onClick={() => {
-              dispatch({
-                type: ActionType.CHANGE_NUMBER_OF_PEOPLE,
-                payload: state.numberOfPeople + 1,
-              });
+              setState((previousState) => ({
+                ...previousState,
+                numberOfPeople: previousState.numberOfPeople + 1,
+              }));
             }}
           />
         </Cell>
@@ -308,36 +234,39 @@ function CalcPage() {
           <DecrementButton
             aria-label="decrement each person pays"
             onClick={() => {
-              const current = currency(state.eachPersonPays, {
-                increment: 1,
-              });
+              const current = currency(eachPersonPays, { increment: 1 });
               const previousEvenDollar = getPreviousEvenDollar(current);
-              const minPerPerson = currency(billAmount).distribute(state.numberOfPeople)[0].value;
-              dispatch({
-                type: ActionType.CHANGE_EACH_PERSON_PAYS,
-                payload: previousEvenDollar < minPerPerson ? minPerPerson : previousEvenDollar,
-              });
+              const minPerPerson = currency(billAmount).divide(state.numberOfPeople).value;
+              setState((previousState) => ({
+                ...previousState,
+                tipSource: {
+                  type: 'perPerson',
+                  value: previousEvenDollar < minPerPerson ? minPerPerson : previousEvenDollar,
+                },
+              }));
             }}
           />
           <CalcInput
             id="each-person-pays"
             name="each-person-pays"
-            value={currency(state.eachPersonPays, { symbol: '' }).format()}
+            value={currency(eachPersonPays, { symbol: '' }).format()}
             onChange={(e) => {
-              dispatch({
-                type: ActionType.CHANGE_EACH_PERSON_PAYS,
-                // TODO: deal with manually setting below minPerPerson
-                payload: toNumber(toCurrency(e.target.value)),
-              });
+              setState((previousState) => ({
+                ...previousState,
+                tipSource: { type: 'perPerson', value: toNumber(toCurrency(e.target.value)) },
+              }));
             }}
           />
           <IncrementButton
             aria-label="increment each person pays"
             onClick={() => {
-              dispatch({
-                type: ActionType.CHANGE_EACH_PERSON_PAYS,
-                payload: currency(state.eachPersonPays, { increment: 1 }).add(1).dollars(),
-              });
+              setState((previousState) => ({
+                ...previousState,
+                tipSource: {
+                  type: 'perPerson',
+                  value: currency(eachPersonPays, { increment: 1 }).add(1).dollars(),
+                },
+              }));
             }}
           />
         </Cell>
@@ -361,7 +290,7 @@ function HeroCell(props: CellProps) {
   const { className, ...otherProps } = props;
   return (
     <Cell
-      className={clsx('border-t border-b border-black py-10 font-semibold', className)}
+      className={clsx('border-y border-black py-10 font-semibold', className)}
       {...otherProps}
     />
   );
@@ -393,10 +322,7 @@ function IncrementButton(props: React.ComponentProps<'button'>) {
 function CalcInput(props: React.ComponentProps<'input'>) {
   const { className, ...otherProps } = props;
   return (
-    <NumericInput
-      className={clsx('mx-2 max-w-[5rem] text-xl md:text-xl', className)}
-      {...otherProps}
-    />
+    <NumericInput className={clsx('mx-2 max-w-20 text-xl md:text-xl', className)} {...otherProps} />
   );
 }
 
